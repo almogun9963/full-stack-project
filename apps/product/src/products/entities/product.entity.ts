78cf6ad628cc89f5d171a6a2550015a2f430b2d9
almog <almogaga2000@gmail.com>
2026-05-19 10:51:06 +0300

    after

--- apps/product/src/products/entities/product.entity.ts
+++ apps/product/src/products/entities/product.entity.ts
@@ -2,18 +2,6 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Product {
-  // @Field(() => Int, { description: 'Example field (placeholder)' })
-  // exampleField: number;
-
-  @Field
-  id: number;
-
-  @Field
-  name: string;
-
-  @Field
-  type: string;
-
-  @Field
-  company: string;
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  exampleField: number;
 }
--- apps/product/src/products/products.module.ts
+++ apps/product/src/products/products.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsResolver } from './products.resolver';
-import { GraphQLModule } from '@nestjs/graphql';
-import {
-  ApolloFederationDriver,
-  ApolloFederationDriverConfig,
-} from '@nestjs/apollo';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-      driver: ApolloFederationDriver,
-      autoSchemaFile: {
-        federation: 2,
-      },
-    }),
-  ],
   providers: [ProductsResolver, ProductsService],
 })
 export class ProductsModule {}
