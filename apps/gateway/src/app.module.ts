b17e68149561be563422d2ffd9db899d722bbd79
almog <almogaga2000@gmail.com>
2026-05-31 17:32:56 +0300

    auth works also in gateway

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -3,32 +3,41 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
+      server: {
+        context: ({ req }) => ({ headers: req.headers }),
+      },
       gateway: {
         supergraphSdl: new IntrospectAndCompose({
           subgraphs: [
-            {
-              name: 'product',
-              url: 'http://localhost:3001/graphql',
-            },
-            {
-              name: 'cart',
-              url: 'http://localhost:3002/graphql',
-            },
-            {
-              name: 'order',
-              url: 'http://localhost:3003/graphql',
-            },
-            {
-              name: 'user',
-              url: 'http://localhost:3004/graphql',
-            },
+            { name: 'product', url: 'http://localhost:3001/graphql' },
+            { name: 'cart', url: 'http://localhost:3002/graphql' },
+            { name: 'order', url: 'http://localhost:3003/graphql' },
+            { name: 'user', url: 'http://localhost:3004/graphql' },
           ],
         }),
+        buildService({ url }) {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+              const incomingHeaders = (context as any).headers;
+
+              if (incomingHeaders) {
+                if (incomingHeaders.authorization) {
+                  request?.http?.headers.set(
+                    'authorization',
+                    incomingHeaders.authorization,
+                  );
+                }
+              }
+            },
+          });
+        },
       },
     }),
   ],
