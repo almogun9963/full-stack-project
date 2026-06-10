25a07e05c524680b726f001787c0d940251c4f60
almog <almogaga2000@gmail.com>
2026-06-10 13:59:22 +0300

    all work

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { Request } from 'express';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { Module } from "@nestjs/common";
+import { Request } from "express";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from "@nestjs/apollo";
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from "@apollo/gateway";
 
 @Module({
   imports: [
@@ -16,10 +16,10 @@ import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
       gateway: {
         supergraphSdl: new IntrospectAndCompose({
           subgraphs: [
-            { name: 'product', url: 'http://localhost:3001/graphql' },
-            { name: 'cart', url: 'http://localhost:3002/graphql' },
-            { name: 'order', url: 'http://localhost:3003/graphql' },
-            { name: 'user', url: 'http://localhost:3004/graphql' },
+            { name: "product", url: "http://localhost:3001/graphql" },
+            { name: "cart", url: "http://localhost:3002/graphql" },
+            { name: "order", url: "http://localhost:3003/graphql" },
+            { name: "user", url: "http://localhost:3004/graphql" },
           ],
         }),
         buildService({ url }) {
@@ -40,7 +40,7 @@ import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 
               if (incomingHeaders?.authorization) {
                 request?.http?.headers.set(
-                  'authorization',
+                  "authorization",
                   incomingHeaders.authorization,
                 );
               }
