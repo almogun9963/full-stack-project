cd4e8a7e09740933b2287bd2617ea2806c36376b
almog <almogaga2000@gmail.com>
2026-06-14 18:02:27 +0300

    updated the time

--- apps/auth/src/user/user.repository.ts
+++ apps/auth/src/user/user.repository.ts
@@ -27,7 +27,7 @@ export class UserRepository {
   ): Promise<User | null> {
     const refreshTokenEntity = {
       refreshToken: refreshToken,
-      expireAt: new Date(),
+      expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     } as RefreshTokenEntity;
 
     return this.userModel
