4b966e9728c26cd4b184313b734ae8135a95378a
almog <almogaga2000@gmail.com>
2026-07-02 10:27:14 +0300

    fixed footer

--- apps/client/components/footer/footer.tsx
+++ apps/client/components/footer/footer.tsx
@@ -1,7 +1,23 @@
 import style from "./footer.module.scss";
 
 const Footer = () => {
-  return <>footer</>;
+  return (
+    <div className={style.footerBox}>
+      <div className={style.footerUpperText}>
+        <img
+          src="../../src/assets/sheep.png"
+          alt="image of a sheep"
+          width="30"
+          height="35"
+        ></img>
+        <p>Drop Sheeping</p>
+      </div>
+
+      <div className={style.footerBottomText}>
+        <p>kinbo - eCommerce Template @2021. Design by Templatecookie</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
