946623869a8a5c3c8a57a2129b56920e9435399a
almog <almogaga2000@gmail.com>
2026-07-05 10:48:19 +0300

    fixed header styling

--- apps/client/src/components/header/header.tsx
+++ apps/client/src/components/header/header.tsx
@@ -5,60 +5,62 @@ const Header = () => {
     <div className={style.headerBox}>
       <div className={style.upperText}>
         <img
+          className={style.images}
           src="../../src/assets/facebook.png"
           alt="facebook logo"
-          width="30"
-          height="35"
         ></img>
 
         <img
+          className={style.images}
           src="../../src/assets/reddit.png"
           alt="reddit logo"
-          width="30"
-          height="35"
         ></img>
         <img
+          className={style.images}
           src="../../src/assets/youtube.png"
           alt="youtube logo"
-          width="30"
-          height="35"
         ></img>
         <img
+          className={style.images}
           src="../../src/assets/instegram.png"
           alt="instegram logo"
-          width="30"
-          height="35"
         ></img>
         <p> :עקבו אחרינו</p>
       </div>
       <div className={style.bottomText}>
         <div>
           <img
+            className={style.images}
             src="../../src/assets/cart.png"
             alt="cart logo"
-            width="30"
-            height="35"
           ></img>
           <img
+            className={style.images}
             src="../../src/assets/heart.png"
             alt="heart logo"
-            width="30"
-            height="35"
           ></img>
           <img
+            className={style.images}
             src="../../src/assets/person.png"
             alt="person logo"
-            width="30"
-            height="35"
           ></img>
         </div>
-        <textarea> חפשו כאן</textarea>
+        <div className={style.searchContainer}>
+          <img
+            className={style.searchIcon}
+            src="../../src/assets/search.png"
+            alt="search logo"
+          ></img>
+          <textarea
+            className={style.textArea}
+            placeholder="חפשו כאן"
+          ></textarea>
+        </div>
         <div className={style.bottomText}>
           <img
+            className={style.images}
             src="../../src/assets/sheep.png"
             alt="image of a sheep"
-            width="30"
-            height="35"
           ></img>
           <p>Drop Sheeping</p>
         </div>
