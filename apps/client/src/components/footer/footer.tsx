bae942e5fee2d8b9be11fc4bb87ab9e5b6a2295c
almog <almogaga2000@gmail.com>
2026-07-05 10:18:32 +0300

    fix

--- apps/client/src/components/footer/footer.tsx
+++ apps/client/src/components/footer/footer.tsx
@@ -1,23 +1,7 @@
 import style from "./footer.module.scss";
 
 const Footer = () => {
-  return (
-    <div className={style.footerBox}>
-      <div className={style.footerUpperText}>
-        <img
-          src="../../src/assets/sheep.png"
-          alt="image of a sheep"
-          width="30"
-          height="35"
-        ></img>
-        <p>Drop Sheeping</p>
-      </div>
-
-      <div className={style.footerBottomText}>
-        <p>kinbo - eCommerce Template @2021. Design by Templatecookie</p>
-      </div>
-    </div>
-  );
+  return <>footer</>;
 };
 
 export default Footer;
