33d091ce94e32e812f9502fea67ef9d29c49f6ea
almog <almogaga2000@gmail.com>
2026-07-06 16:07:58 +0300

    added routing to single product

--- apps/client/src/components/productLogo/product-logo.tsx
+++ apps/client/src/components/productLogo/product-logo.tsx
@@ -1,19 +1,26 @@
 import type { ProductType } from "@repo/common-types";
 import styles from "./product-logo.module.scss";
+import { useNavigate } from "react-router-dom";
 
 interface ProductLogoProps {
   product: ProductType;
 }
 
 export default function ProductLogo(props: ProductLogoProps) {
-  const { name, price, ratings, imageUrl } = props.product;
+  const { id, name, price, ratings, imageUrl } = props.product;
+  const navigate = useNavigate();
 
   const averageRating = ratings?.length
     ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length
     : 0;
 
   return (
-    <div className={styles.productCard}>
+    <div
+      className={styles.productCard}
+      onClick={() => {
+        navigate("/product/" + id);
+      }}
+    >
       <img className={styles.productImage} src={imageUrl} alt={name} />
       <div className={styles.productDetails}>
         <h3 className={styles.productName}>{name}</h3>
