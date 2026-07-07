5ef918aa20788037736fef74b0e7def695192ef2
almog <almogaga2000@gmail.com>
2026-07-07 11:36:33 +0300

    fix

--- packages/common-auth/src/auth.guard.ts
+++ packages/common-auth/src/auth.guard.ts
@@ -38,7 +38,6 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
-      jwt.verify(token, secret);
       const decoded = jwt.verify(token, secret) as {
         id?: string;
         username?: string;
